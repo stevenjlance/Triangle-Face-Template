fd1b3cef43551fa4b729b5ebd44669d2fec5d253
StevenLance <44912347+stevenjlance@users.noreply.github.com>
2021-09-12 21:33:25 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,28 @@
+//Everyone in your break out room will be working on this TRIANGLE face shape. 
+
+//Feel free to style the face template and add to it, but don't change or move the base face triangle - you'll see why tomorrow.
+
+//Pick a different emotion/mood/character for everyone in your group to create and then fork this project and get started. :)
+
+function setup() {
+  createCanvas(400, 400);
+
+}
+
+function draw() {
+  background(220);
+  text(mouseX + ", " + mouseY, 20,20)
+
+  //base face shape
+  triangle(30,300,200,30,370,300)
+
+
+
+
+
+
+
+
+
+
+}
